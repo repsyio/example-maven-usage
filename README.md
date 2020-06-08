91e682a9c06c42164d47f30b384618ee3d7822c4
firatkucuk <firat@fria.io>
2020-06-08 22:15:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This example shows how to usa a java library in a [Repsy](https://repsy.io) private maven repository. 
+
+You can find the original article from:
+
+https://docs.repsy.io/using-private-maven-repository-with-m2-settings
---
+++ src/main/java/io/repsy/example/maven_usage_with_m2_settings/Main.java
@@ -0,0 +1,10 @@
+package io.repsy.example.maven_usage_with_m2_settings;
+
+import io.repsy.example.maven_deploy_with_m2_settings.Hello;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Hello.sayHello("World"));
+    }
+}
